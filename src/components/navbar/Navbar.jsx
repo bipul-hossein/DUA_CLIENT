16da4b4b00616b125d62333580637d946fedae83
bipul-hossein <bipul.pgmr@gmail.com>
2024-12-04 00:26:07 +0600

    reload problem try to sloved

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -59,7 +59,7 @@ export default function Navbar() {
                     {/* Logo */}
                     <div className="w-[135px]">
                         <Link to="/" className="">
-                            <img src="/logo1.jpg" alt="logo" />
+                            <img src="/logo2.png" alt="logo" />
                         </Link>
                     </div>
 
