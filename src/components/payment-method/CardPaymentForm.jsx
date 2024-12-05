cc67b97f985baf2755e0b9db4e5af80ab21c0cb9
bipul-hossein <bipul.pgmr@gmail.com>
2024-12-05 19:43:21 +0600

    update cardPaymentForm and paymemtSuccess page

--- src/components/payment-method/CardPaymentForm.jsx
+++ src/components/payment-method/CardPaymentForm.jsx
@@ -1,236 +1,39 @@
-import { useContext, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { useStripe, useElements, CardElement } from '@stripe/react-stripe-js';
-import { RegistrationContext } from '../../contextsApi/RegistrationContext';
+import React, { useCallback } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import {
+    EmbeddedCheckoutProvider,
+    EmbeddedCheckout,
+} from "@stripe/react-stripe-js";
 
-export default function CardPaymentForm() {
-    const [formDataContext, setFormDataContext] = useContext(RegistrationContext);
-    const stripe = useStripe();
-    const elements = useElements();
-    const { register, handleSubmit, formState: { errors }, reset } = useForm();
-    const [localData, setLocalData] = useState({
-        fullName: '',
-        email: '',
-        cardNumber: '',
-        expirationMonth: '',
-        expirationYear: '',
-        cvv: '',
-        payAmount: '',
-        billingAddress: {
-            address: '',
-            city: '',
-            state: '',
-            zip: '',
-        }
-    });
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
+// pk_test_51M65RLSIrCWJQylGhY5H7fIcgSgxS5xTztCkFIc2bnmTw1Uj4wjFnwVEbYg1DUdI7pEBc7fmTTHrJye8CESMtHJ000YHQOk1rG
 
-    const onSubmit = async (data) => {
-        if (!stripe || !elements) {
-            return;
-        }
-        const cardElement = elements.getElement(CardElement);
-        const { error, paymentMethod } = await stripe.createPaymentMethod({
-            type: 'card',
-            card: cardElement,
-            billing_details: {
-                name: localData.fullName,
-                email: localData.email,
-                address: {
-                    line1: localData.billingAddress.street,
-                    city: localData.billingAddress.city,
-                    state: localData.billingAddress.state,
-                    postal_code: localData.billingAddress.zip,
-                },
-            },
-        });
-
-        if (error) {
-            console.error('Payment error:', error);
-        } else {
-            console.log('Payment method:', paymentMethod);
-        }
-        reset();
-    };
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setLocalData(prev => ({
-            ...prev,
-            [name]: value
-        }));
-    };
-
-    const handleBillingChange = (e) => {
-        const { name, value } = e.target;
-        setLocalData(prev => ({
-            ...prev,
-            billingAddress: {
-                ...prev.billingAddress,
-                [name]: value
+const CardPaymentForm = () => {
+    // Create a Checkout Session
+    const fetchClientSecret = useCallback(() => {
+        return fetch(
+            `${import.meta.env.VITE_API_URL}/event/create-checkout-session`,
+            {
+                method: "POST",
             }
-        }));
-    };
+        )
+            .then((res) => res.json())
+            .then((data) => data.clientSecret);
+    }, []);
+
+    const options = { fetchClientSecret };
 
     return (
-        <div className="">
-            <div className="w-full max-w-lg mx-auto space-y-3 md:space-y-6 p-4 md:p-6 rounded-md">
-                <h1 className="text-xl font-semibold text-gray-900">Pay with Card</h1>
-                <form onSubmit={handleSubmit(onSubmit)} className="space-y-3 md:space-y-4">
-                    <div className="relative">
-                        <label htmlFor="cardNumber" className="block text-sm font-medium text-gray-700 mb-1">
-                            Credit Card Number :
-                        </label>
-                        <CardElement
-                            id="cardNumber"
-                            name="cardNumber"
-                            className="w-full text-gray-600 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-[#13679f]"
-                        />
-                    </div>
-
-                    <div>
-                        <label htmlFor="fullName" className="block text-sm font-medium text-gray-700 mb-1">
-                            Full Name :
-                        </label>
-                        <input
-                            type="text"
-                            id="fullName"
-                            name="fullName"
-                            value={localData.fullName}
-                            {...register('fullName', { required: 'Full Name is required' })}
-                            onChange={handleChange}
-                            className="w-full text-gray-600 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-[#13679f]"
-                            placeholder="Terry B."
-                        />
-                        {errors.fullName && (
-                            <div className="mt-1 text-sm text-red-500">
-                                {errors.fullName.message}
-                            </div>
-                        )}
-                    </div>
-
-                    <div>
-                        <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-1">
-                            Email :
-                        </label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            value={localData.email}
-                            {...register('email', { required: 'Email is required' })}
-                            onChange={handleChange}
-                            className="w-full text-gray-600 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-[#13679f]"
-                            placeholder="terry@example.com"
-                        />
-                        {errors.email && (
-                            <div className="mt-1 text-sm text-red-500">
-                                {errors.email.message}
-                            </div>
-                        )}
-                    </div>
-
-                    <div>
-                        <label htmlFor="billingAddress" className="block text-sm font-medium text-gray-700 mb-1">
-                            Billing Address :
-                        </label>
-                        <div className="flex gap-4">
-                            <div className="w-full md:w-1/2">
-                                <label htmlFor="address" className="block text-sm font-medium text-gray-700">
-                                    Address :
-                                </label>
-                                <input
-                                    type="text"
-                                    id="address"
-                                    name="address"
-                                    value={localData.billingAddress.address}
-                                    {...register('address', { required: 'Address is required' })}
-                                    onChange={handleBillingChange}
-                                    className="w-full text-gray-600 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-[#13679f]"
-                                    placeholder="123 Main St"
-                                />
-                                {errors.address && (
-                                    <div className="mt-1 text-sm text-red-500">
-                                        {errors.address.message}
-                                    </div>
-                                )}
-                            </div>
-
-                            <div className="w-full md:w-1/2">
-                                <label htmlFor="city" className="block text-sm font-medium text-gray-700">
-                                    City :
-                                </label>
-                                <input
-                                    type="text"
-                                    id="city"
-                                    name="city"
-                                    value={localData.billingAddress.city}
-                                    {...register('city', { required: 'City is required' })}
-                                    onChange={handleBillingChange}
-                                    className="w-full text-gray-600 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-[#13679f]"
-                                    placeholder="Anytown"
-                                />
-                                {errors.city && (
-                                    <div className="mt-1 text-sm text-red-500">
-                                        {errors.city.message}
-                                    </div>
-                                )}
-                            </div>
-                        </div>
-
-                        <div className="flex gap-4 mt-4">
-                            <div className="w-full md:w-1/2">
-                                <label htmlFor="state" className="block text-sm font-medium text-gray-700">
-                                    State :
-                                </label>
-                                <input
-                                    type="text"
-                                    id="state"
-                                    name="state"
-                                    value={localData.billingAddress.state}
-                                    {...register('state', { required: 'State is required' })}
-                                    onChange={handleBillingChange}
-                                    className="w-full text-gray-600 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-[#13679f]"
-                                    placeholder="Anystate"
-                                />
-                                {errors.state && (
-                                    <div className="mt-1 text-sm text-red-500">
-                                        {errors.state.message}
-                                    </div>
-                                )}
-                            </div>
-
-                            <div className="w-full md:w-1/2">
-                                <label htmlFor="zip" className="block text-sm font-medium text-gray-700">
-                                    Zip Code :
-                                </label>
-                                <input
-                                    type="text"
-                                    id="zip"
-                                    name="zip"
-                                    value={localData.billingAddress.zip}
-                                    {...register('zip', { required: 'Zip Code is required' })}
-                                    onChange={handleBillingChange}
-                                    className="w-full text-gray-600 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-[#13679f]"
-                                    placeholder="12345"
-                                />
-                                {errors.zip && (
-                                    <div className="mt-1 text-sm text-red-500">
-                                        {errors.zip.message}
-                                    </div>
-                                )}
-                            </div>
-                        </div>
-                    </div>
-
-                    <button
-                        type="submit"
-                        className="w-full bg-[#14649b] text-gray-100 py-2 px-4 rounded-md hover:bg-[#0062BD] transition-colors duration-200 mt-6"
-                        disabled={!stripe || !elements}
-                    >
-                        pay ${formDataContext?.teamFee || 0}
-                    </button>
-                </form>
+        <div className="w-full">
+            <div className="w-full mx-auto space-y-3 md:space-y-6 p-4 md:p-6 rounded-md">
+                <div id="checkout" style={{ width: "100%" }}>
+                    <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+                        <EmbeddedCheckout />
+                    </EmbeddedCheckoutProvider>
+                </div>
             </div>
         </div>
     );
-}
+};
+
+export default CardPaymentForm;
--- src/components/payment-method/PaymentSuccess.jsx
+++ src/components/payment-method/PaymentSuccess.jsx
@@ -1,93 +1,112 @@
-import { Link } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { Link, Navigate } from "react-router-dom";
 
-export default function PaymentSuccess() {
-    return (
-        <div className="flex flex-col items-center justify-center md:min-h-screen bg-background">
-            <div className="mx-auto space-y-6 bg-white/20 p-4 md:p-6 rounded-md px-4">
-                <div className="flex flex-col items-center justify-center space-y-4">
-                    <div className="bg-green-500 rounded-full p-4">
-                        <CheckIcon className="h-8 w-8 text-gray-100" />
-                    </div>
-                    <h1 className="text-3xl font-bold">Payment Successful</h1>
-                    <p className="text-muted-foreground">
-                        Thank you for your purchase! Your transaction was completed successfully.
-                    </p>
-                </div>
-                <div>
-                    <div>
-                        <h3>Order Summary</h3>
-                    </div>
-                    <div className="grid gap-4">
-                        <div className="grid grid-cols-2 items-center">
-                            <span className="text-muted-foreground">Total:</span>
-                            <span className="text-right font-medium">$99.00</span>
-                        </div>
-                        <div className="grid grid-cols-2 items-center">
-                            <span className="text-muted-foreground">Payment Method:</span>
-                            <div className="flex items-center gap-2 justify-end">
-                                <CreditCardIcon className="h-5 w-5" />
-                                <span className="font-medium">Visa **** 4242</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex justify-between flex-wrap-reverse gap-4 mt-4">
-                        <Link
-                            to="/"
-                            className="bg-[#14649b] text-gray-100 py-2 px-4 rounded-md hover:bg-[#0062BD] transition-colors"
-                            prefetch={false}
-                        >
-                            Return to Home
-                        </Link>
-                        <Link
-                            to=""
-                            className="bg-[#14649b] text-gray-100 py-2 px-4 rounded-md hover:bg-[#0062BD] transition-colors"
-                            prefetch={false}
-                        >
-                            Download payment receipt
-                        </Link>
-                    </div>
-                </div>
-            </div>
-        </div>
+const PaymentSuccess = () => {
+  const [status, setStatus] = useState(null);
+  const [customerEmail, setCustomerEmail] = useState("");
+
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const sessionId = urlParams.get("session_id");
+
+    fetch(
+      `${import.meta.env.VITE_API_URL
+      }/event/session-status?session_id=${sessionId}`
     )
-}
+      .then((res) => res.json())
+      .then((data) => {
+        setStatus(data.status);
+        setCustomerEmail(data.customer_email);
+      });
+  }, []);
+
+  if (status === "open") {
+    return <Navigate to="/badminton/registration/payment-method" />;
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center md:min-h-screen bg-background">
+      <div className="mx-auto space-y-6 bg-white/20 p-4 md:p-6 rounded-md px-4">
+        <div className="flex flex-col items-center justify-center space-y-4">
+          <div className="bg-green-500 rounded-full p-4">
+            <CheckIcon className="h-8 w-8 text-gray-100" />
+          </div>
+          <h1 className="text-3xl font-bold">Payment Successful</h1>
+          <p className="text-muted-foreground">
+            Thank you for your purchase! Your transaction was completed
+            successfully.
+          </p>
+        </div>
+        <div>
+          <div>
+            <h3>Order Summary</h3>
+          </div>
+          {/* <div className="grid gap-4">
+            <div className="grid grid-cols-2 items-center">
+              <span className="text-muted-foreground">Total:</span>
+              <span className="text-right font-medium">$99.00</span>
+            </div>
+          </div> */}
+          <div className="flex justify-between flex-wrap-reverse gap-4 mt-4">
+            <Link
+              to="/"
+              className="bg-[#14649b] text-gray-100 py-2 px-4 rounded-md hover:bg-[#0062BD] transition-colors"
+              prefetch={false}
+            >
+              Return to Home
+            </Link>
+            <Link
+              to=""
+              className="bg-[#14649b] text-gray-100 py-2 px-4 rounded-md hover:bg-[#0062BD] transition-colors"
+              prefetch={false}
+            >
+              Download payment receipt
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 function CheckIcon(props) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <path d="M20 6 9 17l-5-5" />
-        </svg>
-    )
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M20 6 9 17l-5-5" />
+    </svg>
+  );
 }
-
 
 function CreditCardIcon(props) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <rect width="20" height="14" x="2" y="5" rx="2" />
-            <line x1="2" x2="22" y1="10" y2="10" />
-        </svg>
-    )
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <rect width="20" height="14" x="2" y="5" rx="2" />
+      <line x1="2" x2="22" y1="10" y2="10" />
+    </svg>
+  );
 }
+
+export default PaymentSuccess;
