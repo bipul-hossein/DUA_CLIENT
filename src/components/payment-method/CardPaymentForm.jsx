c0aaf9806bf6de9a1ecd8928a056a64da679c631
jubayer44 <jubayerahmedshamim44@gmail.com>
2024-12-08 14:43:07 +0600

    update-payment-form

--- src/components/payment-method/CardPaymentForm.jsx
+++ src/components/payment-method/CardPaymentForm.jsx
@@ -1,4 +1,94 @@
-import React, { useCallback, useEffect } from "react";
+// import React, { useCallback, useEffect } from "react";
+// import { loadStripe } from "@stripe/stripe-js";
+// import {
+//   EmbeddedCheckoutProvider,
+//   EmbeddedCheckout,
+// } from "@stripe/react-stripe-js";
+// import { useNavigate } from "react-router-dom";
+
+// const stripePromise = loadStripe(
+//   "pk_test_51M65RLSIrCWJQylGhY5H7fIcgSgxS5xTztCkFIc2bnmTw1Uj4wjFnwVEbYg1DUdI7pEBc7fmTTHrJye8CESMtHJ000YHQOk1rG"
+// );
+// // pk_test_51M65RLSIrCWJQylGhY5H7fIcgSgxS5xTztCkFIc2bnmTw1Uj4wjFnwVEbYg1DUdI7pEBc7fmTTHrJye8CESMtHJ000YHQOk1rG
+
+// const CardPaymentForm = ({ formDataContext }) => {
+//   const navigate = useNavigate();
+
+//   const playerInfo = formDataContext?.personalInfo;
+
+//   const bodyData = {
+//     teamName: playerInfo?.teamName,
+//     division: playerInfo?.division?.toUpperCase(),
+//     amount: formDataContext?.teamFee,
+//     player1Name: playerInfo?.player1FullName,
+//     player2Name: playerInfo?.player2FullName,
+//     player1Email: playerInfo?.player1Email,
+//     player2Email: playerInfo?.player2Email,
+//     player1Phone: playerInfo?.player1PhoneNumber || "",
+//     player2Phone: playerInfo?.player2PhoneNumber || "",
+//     event: "Badminton Registration",
+//   };
+
+//   // Create a Checkout Session
+//   const fetchClientSecret = useCallback(async () => {
+//     try {
+//       const response = await fetch(
+//         `http://localhost:5000/api/event/create-checkout-session`,
+//         {
+//           method: "POST",
+//           headers: {
+//             "Content-Type": "application/json",
+//           },
+//           body: JSON.stringify(bodyData),
+//         }
+//       );
+
+//       if (!response.ok) {
+//         throw new Error(
+//           `Failed to create checkout session: ${response.statusText}`
+//         );
+//       }
+
+//       const data = await response.json();
+//       if (!data?.data?.clientSecret) {
+//         throw new Error("Client secret is missing in the response");
+//       }
+
+//       return data.data.clientSecret;
+//     } catch (error) {
+//       console.error("Error fetching client secret:", error);
+//       return null; // Return null or handle appropriately to prevent further issues
+//     }
+//   }, [bodyData]);
+
+//   const options = { fetchClientSecret };
+
+//   useEffect(() => {
+//     if (!formDataContext?.personalInfo?.division) {
+//       navigate("/badminton/registration");
+//     }
+//   }, [formDataContext]);
+
+//   if (!options.fetchClientSecret) {
+//     return <h1 className="text-2xl text-center">Loading...</h1>;
+//   }
+
+//   return (
+//     <div className="w-full">
+//       <div className="w-full mx-auto space-y-3 md:space-y-6 p-4 md:p-6 rounded-md">
+//         <div id="checkout" style={{ width: "100%" }}>
+//           <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+//             <EmbeddedCheckout />
+//           </EmbeddedCheckoutProvider>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default CardPaymentForm;
+
+import React, { useCallback, useEffect, useMemo } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import {
   EmbeddedCheckoutProvider,
@@ -6,62 +96,61 @@ import {
 } from "@stripe/react-stripe-js";
 import { useNavigate } from "react-router-dom";
 
-const stripePromise = loadStripe("pk_test_51M65RLSIrCWJQylGhY5H7fIcgSgxS5xTztCkFIc2bnmTw1Uj4wjFnwVEbYg1DUdI7pEBc7fmTTHrJye8CESMtHJ000YHQOk1rG");
-// pk_test_51M65RLSIrCWJQylGhY5H7fIcgSgxS5xTztCkFIc2bnmTw1Uj4wjFnwVEbYg1DUdI7pEBc7fmTTHrJye8CESMtHJ000YHQOk1rG
+const stripePromise = loadStripe(
+  "pk_test_51M65RLSIrCWJQylGhY5H7fIcgSgxS5xTztCkFIc2bnmTw1Uj4wjFnwVEbYg1DUdI7pEBc7fmTTHrJye8CESMtHJ000YHQOk1rG"
+);
 
 const CardPaymentForm = ({ formDataContext }) => {
   const navigate = useNavigate();
 
   const playerInfo = formDataContext?.personalInfo;
 
-  const bodyData = {
-    teamName: playerInfo?.teamName,
-    division: playerInfo?.division?.toUpperCase(),
-    amount: formDataContext?.teamFee,
-    player1Name: playerInfo?.player1FullName,
-    player2Name: playerInfo?.player2FullName,
-    player1Email: playerInfo?.player1Email,
-    player2Email: playerInfo?.player2Email,
-    player1Phone: playerInfo?.player1PhoneNumber || "",
-    player2Phone: playerInfo?.player2PhoneNumber || "",
-    event: "Badminton Registration",
-  };
+  const bodyData = useMemo(
+    () => ({
+      teamName: playerInfo?.teamName,
+      division: playerInfo?.division?.toUpperCase(),
+      // amount: formDataContext?.teamFee,
+      player1Name: playerInfo?.player1FullName,
+      player2Name: playerInfo?.player2FullName,
+      player1Email: playerInfo?.player1Email,
+      player2Email: playerInfo?.player2Email,
+      player1Phone: playerInfo?.player1PhoneNumber || "",
+      player2Phone: playerInfo?.player2PhoneNumber || "",
+      event: "Badminton Registration",
+    }),
+    [playerInfo, formDataContext]
+  );
 
-  // Create a Checkout Session
   const fetchClientSecret = useCallback(() => {
-    return fetch(
-      // `https://newsite.ajkerkhobor.news/api/event/create-checkout-session`,
-      `http://localhost:5000/api/event/create-checkout-session`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json", // Add this header
-        },
-        body: JSON.stringify(bodyData),
-      }
-    )
+    return fetch(`http://localhost:5000/api/event/create-checkout-session`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bodyData),
+    })
       .then((res) => res.json())
       .then((data) => data?.data?.clientSecret);
-  }, []);
+  }, [bodyData]);
 
-  const options = { fetchClientSecret };
+  const options = useMemo(() => ({ fetchClientSecret }), [fetchClientSecret]);
 
   useEffect(() => {
     if (!formDataContext?.personalInfo?.division) {
       navigate("/badminton/registration");
     }
-  }, [formDataContext]);
+  }, [formDataContext, navigate]);
+
+  if (!options) {
+    return <h1 className="text-2xl text-center">Loading</h1>;
+  }
 
   return (
-    <div className="w-full">
-      <div className="w-full mx-auto space-y-3 md:space-y-6 p-4 md:p-6 rounded-md">
-        <div id="checkout" style={{ width: "100%" }}>
-          <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
-            <EmbeddedCheckout />
-          </EmbeddedCheckoutProvider>
-        </div>
+    <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+      <div id="checkout" style={{ width: "100%" }}>
+        <EmbeddedCheckout />
       </div>
-    </div>
+    </EmbeddedCheckoutProvider>
   );
 };
 
--- src/components/payment-method/PaymentSuccess.jsx
+++ src/components/payment-method/PaymentSuccess.jsx
@@ -1,17 +1,30 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link, Navigate } from "react-router-dom";
 
 const PaymentSuccess = () => {
   const [status, setStatus] = useState(null);
   const [customerEmail, setCustomerEmail] = useState("");
+  const isFetched = useRef(false);
+
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const sessionId = urlParams.get("session_id");
+  const teamName = urlParams.get("teamName");
+  const division = urlParams.get("division");
+  const player1Name = urlParams.get("player1Name");
+  const player2Name = urlParams.get("player2Name");
+  const player1Email = urlParams.get("player1Email");
+  const player2Email = urlParams.get("player2Email");
+  const player1Phone = urlParams.get("player1Phone");
+  const player2Phone = urlParams.get("player2Phone");
+  const memo = urlParams.get("memo");
 
   useEffect(() => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const sessionId = urlParams.get("session_id");
+    if (isFetched.current) return; // Prevent multiple fetches
+    isFetched.current = true;
 
     fetch(
-      `http://localhost:5000/api/event/session-status?session_id=${sessionId}`
+      `http://localhost:5000/api/event/session-status?session_id=${sessionId}&teamName=${teamName}&division=${division}&player1Name=${player1Name}&player2Name=${player2Name}&player1Email=${player1Email}&player2Email=${player2Email}&player1Phone=${player1Phone}&player2Phone=${player2Phone}&memo=${memo}`
     )
       .then((res) => res.json())
       .then((data) => {
